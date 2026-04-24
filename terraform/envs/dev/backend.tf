d0f3f7cf18b80488bbc6a5f77a3b3593f5fd7e3d
kishorgavhane.dev@gmail.com <kishorgavhane.dev@gmail.com>
2026-04-24 15:20:10 +0530

    ci.yml update

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "chatapp-terraform-state-182138815046"
+    bucket         = "chatapp-tf-state-182138815046"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "chatapp-terraform-lock"
+    use_lockfile = true
     encrypt        = true
   }
 }
